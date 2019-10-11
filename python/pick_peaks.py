8586a7e2c162a710d9103dc397fb49930038553a
Mladen Milosevic <mldnmilosevic@gmail.com>
2019-10-11 12:56:27 +0200

    python: added pick_peaks

---
+++ python/pick_peaks.py
@@ -0,0 +1,29 @@
+def pick_peaks(arr):
+    pos = []
+    peaks = []
+    
+    if len(arr) == 0:
+        return { "pos": pos, "peaks": peaks }
+    
+    plateau = [arr[1], arr[-1]]
+    
+    for i in range(1, len(arr)-1):
+        if arr[i-1] < arr[i] and arr[i] >= arr[i+1]:
+            if arr[i] == arr[i+1]:
+                if arr[i] != plateau[1]:
+                    can_add = True
+                    for j in range(i+1, len(arr)):
+                        if arr[i] > arr[j]:
+                            break
+                        if arr[i] < arr[j]:
+                            can_add = False
+                            break
+                    if can_add:
+                        pos.append(i)
+                        peaks.append(arr[i])
+            else:
+                pos.append(i)
+                peaks.append(arr[i])
+    
+    return { "pos": pos, "peaks": peaks }
+    
